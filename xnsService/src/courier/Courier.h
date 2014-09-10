451cf0b5dfb9481edff8dded361279417df775a6
hasegawa.yasuhiro <hasegawa.yasuhiro@c4172aaf-fed2-8270-6b4c-69d9219bec64>
2014-09-10 07:08:03 +0000

    Add serialize and deserialize for primitive types (quint*).
    git-svn-id: https://mesa-emulator.googlecode.com/svn/trunk@37 c4172aaf-fed2-8270-6b4c-69d9219bec64

--- xnsService/src/courier/Courier.h
+++ xnsService/src/courier/Courier.h
@@ -6,4 +6,34 @@ typedef quint64 quint48;
 
 #include "ByteBuffer.h"
 
+namespace Courier {
+	void serialize  (ByteBuffer* buffer, const quint48& value) {
+		buffer->put48(value);
+	}
+    void deserialize(ByteBuffer* buffer, quint48& value) {
+		value = buffer->get48();
+    }
+
+	void serialize  (ByteBuffer* buffer, const quint32& value) {
+		buffer->put32(value);
+	}
+    void deserialize(ByteBuffer* buffer, quint32& value) {
+		value = buffer->get32();
+    }
+
+	void serialize  (ByteBuffer* buffer, const quint16& value) {
+		buffer->put16(value);
+	}
+    void deserialize(ByteBuffer* buffer, quint16& value) {
+		value = buffer->get16();
+    }
+
+	void serialize  (ByteBuffer* buffer, const quint8& value) {
+		buffer->put8(value);
+	}
+    void deserialize(ByteBuffer* buffer, quint8& value) {
+		value = buffer->get8();
+    }
+}
+
 #endif
