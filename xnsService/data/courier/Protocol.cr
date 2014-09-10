fd6001ed51bc1264bddeec1494ac7111744dc8c2
hasegawa.yasuhiro <hasegawa.yasuhiro@c4172aaf-fed2-8270-6b4c-69d9219bec64>
2014-09-10 07:05:26 +0000

    Change order of items to eliminate forward reference.
    git-svn-id: https://mesa-emulator.googlecode.com/svn/trunk@35 c4172aaf-fed2-8270-6b4c-69d9219bec64

--- xnsService/data/courier/Protocol.cr
+++ xnsService/data/courier/Protocol.cr
@@ -1,21 +1,8 @@
 Protocol: PROGRAM 0 VERSION 0 =
 BEGIN
 
-ProtocolType:    TYPE = {protocol0(0), protocol1(1), protocol2(2), protocol3(3)};
-MessageType: TYPE = {call(0), reject(1), return(2), abort(3)};
-
-MessageObject: TYPE = CHOICE ProtocolType OF {
-    protocol0 => RECORD[],
-    protocol1 => RECORD[],
-    protocol2 => RECORD[ protocol2Body: Protocol2Body ],
-    protocol3 => RECORD[ protocol3Body: Protocol3Body ]
-};
-
-Protocol2Body: TYPE = CHOICE MessageType OF {
-    call   => RECORD [transaction, program, version, procedure: CARDINAL ],
-    reject => RECORD [transaction: CARDINAL, reject: RejectCode ],
-    return => RECORD [transaction: CARDINAL ],
-    abort  => RECORD [transaction: CARDINAL, abort: CARDINAL ]};
+ProtocolType: TYPE = {protocol0(0), protocol1(1), protocol2(2), protocol3(3)};
+MessageType:  TYPE = {call(0), reject(1), return(2), abort(3)};
 
 RejectCode: TYPE = {
     noSuchProgramNumber(0),
@@ -23,9 +10,20 @@ RejectCode: TYPE = {
     noSuchProcedureValue(2),
     invalidArguments(3)};
 
+Protocol2Body: TYPE = CHOICE MessageType OF {
+    call   => RECORD [transaction, program, version, procedure: CARDINAL],
+    reject => RECORD [transaction: CARDINAL, reject: RejectCode],
+    return => RECORD [transaction: CARDINAL],
+    abort  => RECORD [transaction: CARDINAL, abort: CARDINAL]};
+
+ProtocolRange: TYPE = RECORD [low, high: ProtocolType];
+VersionRange:  TYPE = RECORD [low, high: CARDINAL];
+pvLow:  ProtocolType = protocol2;
+pvHigh: ProtocolType = protocol3;
+
 RejectBody: TYPE = CHOICE RejectCode OF {
     noSuchProgramNumber, noSuchProcedureValue, invalidArguments => RECORD [],
-    noSuchVersionNumber => RECORD [ range: VersionRange]
+    noSuchVersionNumber => RECORD [range: VersionRange]
 };
 
 Protocol3Body: TYPE = CHOICE MessageType OF {
@@ -35,11 +33,13 @@ Protocol3Body: TYPE = CHOICE MessageType OF {
                       procedure:   CARDINAL],
     reject => RECORD [transaction: CARDINAL, rejectBody:  RejectBody],
     return => RECORD [transaction: CARDINAL],
-    abort  => RECORD [transaction: CARDINAL, abort: CARDINAL ]};
+    abort  => RECORD [transaction: CARDINAL, abort: CARDINAL]};
 
-ProtocolRange: TYPE = RECORD [low, high: ProtocolType];
-VersionRange:  TYPE = RECORD [low, high: CARDINAL];
-pvLow:  ProtocolType = protocol2;
-pvHigh: ProtocolType = protocol3;
+MessageObject: TYPE = CHOICE ProtocolType OF {
+    protocol0 => RECORD[],
+    protocol1 => RECORD[],
+    protocol2 => RECORD[ protocol2Body: Protocol2Body],
+    protocol3 => RECORD[ protocol3Body: Protocol3Body]
+};
 
 END.
