7314cc52f1b42aa579908531bb626e14f18e9878
hasegawa.yasuhiro <hasegawa.yasuhiro@c4172aaf-fed2-8270-6b4c-69d9219bec64>
2014-09-12 07:29:40 +0000

    Pass localNetworkNumber in constructor of SocketRouting to register network number of local segment. Add SocketRouting::addNetwork(quint32, quint16) to register other network information.
    git-svn-id: https://mesa-emulator.googlecode.com/svn/trunk@72 c4172aaf-fed2-8270-6b4c-69d9219bec64

--- xnsService/src/courier/socket/SocketRouting.h
+++ xnsService/src/courier/socket/SocketRouting.h
@@ -9,9 +9,15 @@
 
 class SocketRouting : public SocketManager::Socket {
 public:
-	SocketRouting() : SocketManager::Socket("Routing") {}
+	SocketRouting(quint32 localNetworkNumber_) : SocketManager::Socket("Routing"), localNetworkNumber(localNetworkNumber_) {
+		addNetwork(localNetworkNumber, 0);
+	}
+	void addNetwork(quint32 networkNumber, quint16 hop);
+	void process(const SocketManager::Context& context, ByteBuffer& request, ByteBuffer& response);
 
-	void process(ByteBuffer& request, ByteBuffer& response);
+protected:
+	const quint32 localNetworkNumber;
+	QMap<quint32, quint16>networkMap;
 };
 
 #endif
