f7dce08628b4f8391aecf95188fdfec7ed851174
hasegawa.yasuhiro <hasegawa.yasuhiro@c4172aaf-fed2-8270-6b4c-69d9219bec64>
2014-09-17 05:22:26 +0000

    Major rewrite of SocketManager.cpp
    git-svn-id: https://mesa-emulator.googlecode.com/svn/trunk@84 c4172aaf-fed2-8270-6b4c-69d9219bec64

--- xnsService/src/courier/SocketManager.cpp
+++ xnsService/src/courier/SocketManager.cpp
@@ -96,45 +96,150 @@ void SocketManager::SocketThread::run() {
 		if (dataLength <= 0) RUNTIME_ERROR();
 		request.setLimit((quint32)dataLength);
 
-		Courier::Ethernet::Header ethernet;
-		Courier::Datagram::Header datagram;
-
-		Courier::deserialize(request, ethernet);
-		Courier::deserialize(request, datagram);
-		request.setPos(datagram.base + datagram.length);
-		request.rewind();
-		//logger.debug("dataLength = %d  newLimit = %u", dataLength, request.getLimit());
-
-		quint16 socketNo = datagram.destination.socket;
-		Courier::Datagram::PacketType packetType = (Courier::Datagram::PacketType)(datagram.flags & 0xff);
-
 		Context context(socketManager.localNetworkNumber, socketManager.network.getAddress());
 
-		SocketManager::Socket* listener = socketManager.map.value(socketNo, 0);
-		if (listener) {
-			logger.debug("ETHER     %012llX  %012llX  %04X", ethernet.destination, ethernet.source, ethernet.type);
-			logger.debug("DATAGRAM  %04X %4d %02X %s  %08X-%012llX-%s  %08X-%012llX-%s",
-				datagram.checksum, datagram.length, (datagram.flags >> 8), Courier::getName(packetType),
-				datagram.destination.network, datagram.destination.host, Courier::getSocketName(datagram.destination.socket),
-				datagram.source.network, datagram.source.host, Courier::getSocketName(datagram.source.socket));
+		// make alias of fields in context
+		Courier::Ethernet::Header& reqEthernet = context.reqEthernet;
+		Courier::Datagram::Header& reqDatagram = context.reqDatagram;
+		Courier::Ethernet::Header& resEthernet = context.resEthernet;
+		Courier::Datagram::Header& resDatagram = context.resDatagram;
 
-			logger.debug("====");
+		Courier::deserialize(request, reqEthernet);
+		Courier::deserialize(request, reqDatagram);
+		request.setLimit(reqDatagram.base + reqDatagram.length);
+		//logger.debug("dataLength = %d  newLimit = %u", dataLength, request.getLimit());
 
-			// Call listener if exists.
-			QMutexLocker mutexLocker(&socketManager.mutex);
-			listener->process(context, request, response);
-			if (response.getLimit()) {
-				quint8* data    = response.getData();
-				quint32 dataLen = response.getLimit();
-				int opErrno     = 0;
-				int ret = socketManager.network.transmit(data, dataLen, opErrno);
-				if (opErrno) {
-					logger.fatal("send fail opErrno = %d  ret = %d  dataLen = %d", opErrno, ret, dataLen);
-					RUNTIME_ERROR();
+		try {
+			// check integrity of packet
+			if (reqDatagram.checksum != Courier::checksum(request.getData(), reqDatagram.base + 2, reqDatagram.length - 2)) {
+				// packet data is corrupted
+				throw PacketError(Courier::Error::ErrorNumber::BAD_CHECKSUM);
+			}
+
+			// check destination network
+			switch(reqDatagram.destination.network) {
+			case Courier::Datagram::NETWORK_UNKNOWN:
+			case Courier::Datagram::NETWORK_ALL:
+				break;
+			default:
+				// If packet is for local network
+				if (reqDatagram.destination.network == context.network) break;
+				// packet is for other network
+				// TODO forward this packet
+				logger.warn("FORWARD %08X-%012llX-%s", reqDatagram.destination.network, reqDatagram.destination.host, Courier::getSocketName(reqDatagram.destination.socket));
+				continue;
+			}
+
+			// Build response
+			response.clear();
+
+			// Build response ethernet
+			resEthernet.base        = reqEthernet.base;
+			resEthernet.destination = reqEthernet.source;
+			resEthernet.source      = context.host;
+			resEthernet.type        = reqEthernet.type;
+			serialize(response, resEthernet);
+
+			// Build response datagram (partial)
+			resDatagram.base                = reqDatagram.base;
+			resDatagram.checksum            = 0;
+			resDatagram.length              = 0;
+			resDatagram.flags               = reqDatagram.flags & 0x00ffU;
+			resDatagram.destination.base    = reqDatagram.destination.base;
+			resDatagram.destination.network = reqDatagram.source.network;
+			resDatagram.destination.host    = reqDatagram.source.host;
+			resDatagram.destination.socket  = reqDatagram.source.socket;
+			resDatagram.source.base         = reqDatagram.source.base;
+			resDatagram.source.network      = context.network;
+			resDatagram.source.host         = context.host;
+			resDatagram.source.socket       = reqDatagram.destination.socket;
+			serialize(response, resDatagram);
+
+			SocketManager::Socket* listener = socketManager.map.value(reqDatagram.destination.socket, 0);
+			if (listener) {
+				Courier::Datagram::PacketType reqPacketType = (Courier::Datagram::PacketType)(reqDatagram.flags & 0xff);
+				logger.debug("ETHER     %012llX  %012llX  %04X", reqEthernet.destination, reqEthernet.source, reqEthernet.type);
+				logger.debug("DATAGRAM  %04X %4d %02X %s  %08X-%012llX-%s  %08X-%012llX-%s",
+					reqDatagram.checksum, reqDatagram.length, (reqDatagram.flags >> 8), Courier::getName(reqPacketType),
+					reqDatagram.destination.network, reqDatagram.destination.host, Courier::getSocketName(reqDatagram.destination.socket),
+					reqDatagram.source.network, reqDatagram.source.host, Courier::getSocketName(reqDatagram.source.socket));
+
+				logger.debug("====");
+
+				// Call listener if exists.
+				QMutexLocker mutexLocker(&socketManager.mutex);
+				listener->process(context, request, response);
+			} else {
+				// no one listen the socket
+				throw PacketError(Courier::Error::ErrorNumber::NO_SOCKET);
+			}
+		} catch (PacketError& packetError) {
+			// Don't send error packet of broadcast request
+			if (reqDatagram.destination.network == Courier::Datagram::NETWORK_ALL) continue;
+			if (reqDatagram.destination.host    == Courier::Datagram::HOST_ALL)    continue;
+
+			// Clear buffer in case data is already written to response
+			response.clear();
+
+			// Build response
+			serialize(response, resEthernet);
+
+			// Build response
+			resDatagram.flags               = (quint16)Courier::Datagram::PacketType::ERROR;
+			serialize(response, resDatagram);
+
+			// Build response
+			Courier::Error::Header resError;
+			resError.base      = response.getPos();
+			resError.number    = packetError.errorNumber;
+			resError.parameter = packetError.errorParameter;
+			serialize(response, resError);
+
+			// output request packet data from datagram
+			quint8* p = request.getData() + reqDatagram.base;
+			//Courier::checksum(request.getData(), datagram.base + 2, datagram.length - 2)
+			for(quint32 i = response.getPos(); i < reqDatagram.length; i++) {
+				response.put8(*p++);
+			}
+		}
+
+		// if something is written to response, sent it
+		if (response.getLimit()) {
+			// set length of response datagram
+			resDatagram.length = response.getLimit() - reqDatagram.base;
+
+			// Move position to last written data == limit
+			response.setPos(response.getLimit());
+
+			// if number of datagram size is odd, add one zero byte to make even length
+			if (reqDatagram.length & 1) {
+				response.put8(0);
+			}
+
+			// expand packet size to meet minimum packet length requirement if needed
+			if (response.getLimit() < (reqDatagram.base + Courier::Ethernet::MIN_DATA_LENGTH)) {
+				quint32 padSize = reqDatagram.base + Courier::Ethernet::MIN_DATA_LENGTH - response.getLimit();
+				for(quint32 i = 0; i < padSize; i++) {
+					response.put8(0);
 				}
 			}
-		} else {
-//			logger.debug("----");
+
+			// write resDatagram to response (length and padding)
+			serialize(response, resDatagram);
+
+			// calculate checksum from response
+			resDatagram.checksum = Courier::checksum(response.getData(), resDatagram.base + 2, resDatagram.length - 2);
+
+			// write resDatagram to response
+			serialize(response, resDatagram);
+
+			// finally output response packet using response buffer.
+			int opErrno = 0;
+			int ret = socketManager.network.transmit(response.getData(), response.getLimit(), opErrno);
+			if (opErrno) {
+				logger.fatal("send fail opErrno = %d  ret = %d  resLimit = %d", opErrno, ret, response.getLimit());
+				RUNTIME_ERROR();
+			}
 		}
 	}
 }
