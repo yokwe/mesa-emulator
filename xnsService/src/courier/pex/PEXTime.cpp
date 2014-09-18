7e759498c69bcde5594a0f51575dfd68ea519df8
hasegawa.yasuhiro <hasegawa.yasuhiro@c4172aaf-fed2-8270-6b4c-69d9219bec64>
2014-09-18 07:43:18 +0000

    Add pex/PEXTime.{cpp,h}
    git-svn-id: https://mesa-emulator.googlecode.com/svn/trunk@97 c4172aaf-fed2-8270-6b4c-69d9219bec64

---
+++ xnsService/src/courier/pex/PEXTime.cpp
@@ -0,0 +1,111 @@
+/*
+Copyright (c) 2014, Yasuhiro Hasegawa
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+  3. The name of the author may not be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+
+//
+// PEXTime.cpp
+//
+
+#include "../util/Util.h"
+static log4cpp::Category& logger = Logger::getLogger("pexTime");
+
+#include "../util/Debug.h"
+
+#include "PEXTime.h"
+
+#include "../Courier.h"
+
+#include <QDateTime>
+
+
+// Unix Time Epoch  1970-01-01 00:00:00
+// Mesa Time Epoch  1968-01-01 00:00:00
+//   Difference between above 2 date is 731 days.
+static const quint32 EPOCH_DIFF = Courier::Time2::EARLIEST_TIME + (quint32)(731 * 60 * 60 * 24);
+
+static quint32 toMesaTime(quint32 unixTime) {
+	return unixTime + EPOCH_DIFF;
+}
+
+void PEXTime::process(const Context& /*context*/, ByteBuffer& request, ByteBuffer& response) {
+	using namespace Courier;
+
+	Time2::Packet reqPacket;
+	deserialize(request, reqPacket);
+
+	if (reqPacket.version != Time2::VERSION) {
+		logger.error("Unexpected version = %d", reqPacket.version);
+		throw SocketManager::PacketError(Error::ErrorNumber::UNSPECIFIED);
+	}
+
+	switch(reqPacket.data.tag) {
+	case Time2::PacketType::REQUEST: {
+		QDateTime dateTime;
+
+		QTimeZone timeZone = dateTime.timeZone();
+
+		const quint32 currentTime              = dateTime.toTime_t() + (24 * 60 * 60);
+		const QTimeZone::OffsetData offsetData = timeZone.offsetData(dateTime);
+		const qint32 offset                    = offsetData.offsetFromUtc;
+		const qint32 offsetInMinutes = ::abs(offset) / 60;
+
+		// start of build response
+		Time2::Packet resPacket;
+		resPacket.base      = reqPacket.base;
+		resPacket.version   = Time2::VERSION;
+
+		resPacket.data.base = reqPacket.data.base;
+		resPacket.data.tag  = Time2::PacketType::RESPONSE;
+
+		resPacket.data.RESPONSE.currentTime     = toMesaTime(currentTime);
+		resPacket.data.RESPONSE.offsetDirection = (offset < 0) ? Time2::OffsetDirection::WEST : Time2::OffsetDirection::EAST;
+		resPacket.data.RESPONSE.offsetHours     = offsetInMinutes / 60;
+		resPacket.data.RESPONSE.offsetMinutes   = offsetInMinutes % 60;
+		// We don't care about DST
+		resPacket.data.RESPONSE.startOfDST      = 0;
+		resPacket.data.RESPONSE.endOfDST        = 0;
+		// 10 milliseconds precision
+		resPacket.data.RESPONSE.toleranceType   = Time2::ToleranceType::IN_MILLI_SECONDS;
+		resPacket.data.RESPONSE.tolerance       = 10;
+
+		serialize(response, resPacket);
+
+		// end of building response
+		response.rewind();
+	}
+		break;
+	case Time2::PacketType::RESPONSE:
+		// DO NOTHING
+		logger.fatal("RESPONSE");
+		RUNTIME_ERROR();
+		break;
+	default:
+		logger.fatal("tag = %d", (quint16)reqPacket.data.tag);
+		RUNTIME_ERROR();
+		break;
+	}
+
+}
---
+++ xnsService/src/courier/pex/PEXTime.h
@@ -0,0 +1,18 @@
+//
+// PEXTime.h
+//
+
+#ifndef PEXTIME_H__
+#define PEXTIME_H__
+
+#include "../socket/SocketPEX.h"
+#include "../stub/StubTime2.h"
+
+class PEXTime : public SocketPEX::PEX {
+public:
+	PEXTime() : SocketPEX::PEX("Time") {}
+
+	void process(const Context& context, ByteBuffer& request, ByteBuffer& response);
+};
+
+#endif
