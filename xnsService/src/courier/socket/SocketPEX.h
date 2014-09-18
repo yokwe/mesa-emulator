d348dee8534e8cd9aa1992cd4a05a60b8dafcb44
hasegawa.yasuhiro <hasegawa.yasuhiro@c4172aaf-fed2-8270-6b4c-69d9219bec64>
2014-09-18 07:42:10 +0000

    Include PEX in SocketPEX.
    git-svn-id: https://mesa-emulator.googlecode.com/svn/trunk@95 c4172aaf-fed2-8270-6b4c-69d9219bec64

--- xnsService/src/courier/socket/SocketPEX.h
+++ xnsService/src/courier/socket/SocketPEX.h
@@ -8,26 +8,26 @@
 #include "../SocketManager.h"
 #include "../stub/StubPacketExchange.h"
 
-class PEX {
-public:
-	class Context : public SocketManager::Socket::Context {
-	public:
-		Courier::PacketExchange::Header reqPEX;
-		Courier::PacketExchange::Header resPEX;
-
-		Context(const SocketManager::Socket::Context& socketContext_) : SocketManager::Socket::Context(socketContext_) {}
-	};
-
-	const char* name;
-	//
-	PEX(const char* name_) : name(name_) {}
-	virtual ~PEX() {}
-
-	virtual void process(const Context& context, ByteBuffer& request, ByteBuffer& response);
-};
-
 class SocketPEX : public SocketManager::Socket {
 public:
+	class PEX {
+	public:
+		class Context : public SocketManager::Socket::Context {
+		public:
+			Courier::PacketExchange::Header reqPEX;
+			Courier::PacketExchange::Header resPEX;
+
+			Context(const SocketManager::Socket::Context& socketContext_) : SocketManager::Socket::Context(socketContext_) {}
+		};
+
+		const char* name;
+		//
+		PEX(const char* name_) : name(name_) {}
+		virtual ~PEX() {}
+
+		virtual void process(const Context& context, ByteBuffer& request, ByteBuffer& response) = 0;
+	};
+
 	SocketPEX(Courier::PacketExchange::ClientType clientType_, PEX* pex_) :
 		SocketManager::Socket("PEX"), clientType(clientType_), pex(pex_) {}
 	void process(const Socket::Context& context, ByteBuffer& request, ByteBuffer& response);
