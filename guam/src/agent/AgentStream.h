b00eaa1c920ab5778f3859cac293e1d8a8b2a442
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-06-16 20:20:51 +0900

    To start new implementaion of AgentStream, move currenr files under src/agent/Attic.

--- guam/src/agent/AgentStream.h
+++ guam/src/agent/AgentStream.h
@@ -32,137 +32,10 @@ OF SUCH DAMAGE.
 #ifndef AGENTSTREAM_H__
 #define AGENTSTREAM_H__
 
-#include <log4cpp/Category.hh>
-
 #include "Agent.h"
-
-#include <QtCore>
 
 class AgentStream : public Agent {
 public:
-	//
-	// Block
-	//
-	class Block {
-	public:
-		Block(CoProcessorIOFaceGuam::TransferRec* mesaPut) {
-			put(mesaPut);
-		}
-		Block(const Block& that) {
-			this->data = that.data;
-		}
-		Block(const CARD32 value) {
-			put32(value);
-		}
-		Block(const QByteArray& data);
-		int getSize() const {
-			return data.size();
-		}
-		CARD32 get32() const;
-		void   put32(CARD32 value);
-		char   at(int i) const;
-
-		QString getIPAddress() const {
-			const CARD32 ipAddress = get32();
-			const CARD8  a = (CARD8)(ipAddress >>  8); // AA
-			const CARD8  b = (CARD8)(ipAddress >>  0); // BB
-			const CARD8  c = (CARD8)(ipAddress >> 24); // CC
-			const CARD8  d = (CARD8)(ipAddress >> 16); // DD
-
-			return QString("%1.%2.%3.%4").arg(a).arg(b).arg(c).arg(d);
-		}
-
-		void get(CoProcessorIOFaceGuam::TransferRec* mesaPut);
-		void put(CoProcessorIOFaceGuam::TransferRec* mesaGet);
-
-		QString toHexString() {
-			QString ret(data.toHex().constData());
-			return ret;
-		}
-		QString toString() {
-			QString ret(data.constData());
-			return ret;
-		}
-		QString toEscapedString() const;
-	private:
-		// data contains byte swapped data
-		QByteArray data;
-	};
-
-
-	//
-	// Task
-	//
-	class Task {
-	protected:
-		static       CARD16              hTaskNext;
-
-		Task() : hTask(++hTaskNext) {
-			addTask(this);
-		}
-
-	public:
-		const CARD16 hTask;
-
-		QList<Block> readList;
-		QList<Block> writeList;
-
-		void debugDump(log4cpp::Category& logger);
-	};
-
-
-	//
-	// Handler
-	//
-	class Handler {
-	public:
-		//ResultType: TYPE = MACHINE DEPENDENT
-		//  {completed(0), inProgress(1), error(2)};
-		enum class ResultType : CARD16 {
-			completed  = CoProcessorIOFaceGuam::R_completed,
-			inProgress = CoProcessorIOFaceGuam::R_inProgress,
-			error      = CoProcessorIOFaceGuam::R_error,
-		};
-
-		static void     initialize();
-		static void     debugDump(log4cpp::Category& logger, const char* name, CoProcessorIOFaceGuam::CoProcessorIOCBType* iocb);
-		static Handler* getHandler(CARD32 serverID);
-
-		const CARD32  serverID;
-		const QString name;
-
-		Handler(CARD32 serverID_, QString name_) : serverID(serverID_), name(name_) {}
-		virtual ~Handler();
-
-		// Return Handler specific Task
-		virtual Task*      createTask() = 0;
-
-		virtual ResultType idle   (CoProcessorIOFaceGuam::CoProcessorIOCBType* iocb) = 0;
-		virtual ResultType accept (CoProcessorIOFaceGuam::CoProcessorIOCBType* iocb) = 0;
-		virtual ResultType connect(CoProcessorIOFaceGuam::CoProcessorIOCBType* iocb) = 0;
-		virtual ResultType destroy(CoProcessorIOFaceGuam::CoProcessorIOCBType* iocb) = 0;
-		virtual ResultType read   (CoProcessorIOFaceGuam::CoProcessorIOCBType* iocb) = 0;
-		virtual ResultType write  (CoProcessorIOFaceGuam::CoProcessorIOCBType* iocb) = 0;
-	};
-
-
-	//
-	// AgentStream
-	//
-	static const char* getServerIDString(CARD32 serverID);
-	static const char* getCommandString (CARD16 command);
-	static const char* getStateString   (CARD16 state);
-	static const char* getResultString  (CARD16 result);
-
-	static void notifyInterrupt();
-
-	static void                  setDefaultHandler(Handler* handler);
-	static void                  addHandler(AgentStream::Handler* handler);
-	static AgentStream::Handler* getHandler(CARD32 serverID_);
-
-	static void                  addTask(Task* task_);
-	static AgentStream::Task*    getTask(CARD16 hTask);
-
 	AgentStream() : Agent(GuamInputOutput::stream, "Stream") {
 		fcb = 0;
 	}
@@ -175,14 +48,7 @@ public:
 	void Call();
 
 private:
-	static QMap<CARD32, Handler*> handlerMap;
-	static Handler*               defaultHandler;
-	static QMap<CARD16, Task*>    taskMap;
-	static CARD16                 interruptSelector;
-
 	CoProcessorIOFaceGuam::CoProcessorFCBType* fcb;
-
-	void initialize();
 };
 
 #endif
--- guam/src/agent/agent.pro
+++ guam/src/agent/agent.pro
@@ -16,9 +16,6 @@ SOURCES += Agent.cpp AgentBeep.cpp AgentDisk.cpp AgentDisplay.cpp AgentFloppy.cp
 HEADERS += AgentMouse.h   AgentNetwork.h   AgentProcessor.h   AgentStream.h   DiskFile.h   NetworkPacket.h
 SOURCES += AgentMouse.cpp AgentNetwork.cpp AgentProcessor.cpp AgentStream.cpp DiskFile.cpp
 
-HEADERS += StreamDefault.h   StreamTcpService.h
-SOURCES += StreamDefault.cpp StreamTcpService.cpp
-
 unix {
 	SOURCES += NetworkPacket.cpp
 }
