2e37351afd81fa21291fffab3c5650750f69cc8b
yokwe <hasegawa.yasuhiro+github.com>
2017-06-11 15:22:35 +0900

    Implements opcode.

--- guam/src/simple-opcode/OpcodeMop3xx.cpp
+++ guam/src/simple-opcode/OpcodeMop3xx.cpp
@@ -44,40 +44,122 @@ static log4cpp::Category& logger = Logger::getLogger("3xx");
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-
+__attribute__((always_inline)) static inline void E_LI_(CARD16 arg) {
+	if (DEBUG_TRACE_RUN) logger.debug("TRACE %6o  LI %5d", savedPC, arg);
+	Push(arg);
+}
+#define LIn(n) \
+void E_LI##n (Opcode*) { \
+	E_LI_(n); \
+}
 // 0300  ASSIGN_MOP(z, LI0)
+LIn(0)
 // 0301  ASSIGN_MOP(z, LI1)
+LIn(1)
 // 0302  ASSIGN_MOP(z, LI2)
+LIn(2)
 // 0303  ASSIGN_MOP(z, LI3)
+LIn(3)
 // 0304  ASSIGN_MOP(z, LI4)
+LIn(4)
 // 0305  ASSIGN_MOP(z, LI5)
+LIn(5)
 // 0306  ASSIGN_MOP(z, LI6)
+LIn(6)
 // 0307  ASSIGN_MOP(z, LI7)
-
+LIn(7)
 // 0310  ASSIGN_MOP(z, LI8)
+LIn(8)
 // 0311  ASSIGN_MOP(z, LI9)
+LIn(9)
 // 0312  ASSIGN_MOP(z, LI10)
+LIn(10)
 // 0313  ASSIGN_MOP(z, LIN1)
+void E_LIN1(Opcode*) {
+	E_LI_((CARD16)0xffff);
+}
 // 0314  ASSIGN_MOP(z, LINI)
+void E_LINI(Opcode*) {
+	E_LI_((CARD16)0x8000);
+}
 // 0315  ASSIGN_MOP(z, LIB)
+void E_LIB(Opcode*) {
+	E_LI_(GetCodeByte());
+}
 // 0316  ASSIGN_MOP(z, LIW)
+void E_LIW(Opcode*) {
+	E_LI_(GetCodeWord());
+}
 // 0317  ASSIGN_MOP(z, LINB)
-
+void E_LINB(Opcode*) {
+	E_LI_((CARD16)(0xff00 | GetCodeByte()));
+}
 // 0320  ASSIGN_MOP(z, LIHB)
+void E_LIHB(Opcode*) {
+	E_LI_((CARD16)(GetCodeByte() << 8));
+}
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+__attribute__((always_inline)) static inline void E_LID_(CARD32 arg) {
+	if (DEBUG_TRACE_RUN) logger.debug("TRACE %6o  LID %5d", savedPC, arg);
+	PushLong(arg);
+}
 // 0321  ASSIGN_MOP(z, LID0)
+void E_LID0(Opcode*) {
+	E_LID_(0);
+}
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+__attribute__((always_inline)) static inline void E_LA_(CARD16 arg) {
+	if (DEBUG_TRACE_RUN) logger.debug("TRACE %6o  LA %5d", savedPC, arg);
+	Push(LFCache::LF() + arg);
+}
+#define LAn(n) \
+void E_LA##n (Opcode*) { \
+	E_LA_(n); \
+}
 // 0322  ASSIGN_MOP(z, LA0)
+LAn(0)
 // 0323  ASSIGN_MOP(z, LA1)
+LAn(1)
 // 0324  ASSIGN_MOP(z, LA2)
+LAn(2)
 // 0325  ASSIGN_MOP(z, LA3)
+LAn(3)
 // 0326  ASSIGN_MOP(z, LA6)
+LAn(6)
 // 0327  ASSIGN_MOP(z, LA8)
-
+LAn(8)
 // 0330  ASSIGN_MOP(z, LAB)
+void E_LAB(Opcode*) {
+	E_LA_(GetCodeByte());
+}
 // 0331  ASSIGN_MOP(z, LAW)
+void E_LAW(Opcode*) {
+	E_LA_(GetCodeWord());
+}
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+__attribute__((always_inline)) static inline void E_GA_(CARD16 arg) {
+	if (DEBUG_TRACE_RUN) logger.debug("TRACE %6o  LA %5d", savedPC, arg);
+	Push((CARD16)(GF + arg));
+}
+#define GAn(n) \
+void E_GA##n (Opcode*) { \
+	E_GA_(n); \
+}
 // 0332  ASSIGN_MOP(z, GA0)
+GAn(0)
 // 0333  ASSIGN_MOP(z, GA1)
+GAn(1)
 // 0334  ASSIGN_MOP(z, GAB)
+void E_GAB(Opcode*) {
+	E_GA_(GetCodeByte());
+}
 // 0335  ASSIGN_MOP(z, GAW)
+void E_GAW(Opcode*) {
+	E_GA_(GetCodeWord());
+}
 // 0336  //ASSIGN_MOP(z, CAW)
 // 0337  ASSIGN_MOP(z, EFC0)
 
@@ -107,12 +189,51 @@ static log4cpp::Category& logger = Logger::getLogger("3xx");
 // 0365  ASSIGN_MOP(z, BLTC)
 // 0366  ASSIGN_MOP(z, BLTCL)
 // 0367  ASSIGN_MOP(z, LP)
-
+void E_LP(Opcode*) {
+	if (DEBUG_TRACE_RUN) logger.debug("TRACE %6o  LP", savedPC);
+	POINTER ptr = Pop();
+	PushLong((ptr == 0) ? 0 : LengthenPointer(ptr));
+}
 // 0370  ASSIGN_MOP(z, ESC)
+void E_ESC(Opcode*) {
+	Interpreter::dispatchEsc(GetCodeByte());
+}
 // 0371  ASSIGN_MOP(z, ESCL)
+void E_ESCL(Opcode*) {
+	Interpreter::dispatchEsc(GetCodeByte());
+}
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+__attribute__((always_inline)) static inline void E_LGA_(CARD16 arg) {
+	if (DEBUG_TRACE_RUN) logger.debug("TRACE %6o  LGA %5d", savedPC, arg);
+	PushLong(GF + arg);
+}
 // 0372  ASSIGN_MOP(z, LGA0)
+void E_LGA0(Opcode*) {
+	E_LGA_(0);
+}
 // 0373  ASSIGN_MOP(z, LGAB)
+void E_LGAB(Opcode*) {
+	E_LGA_(GetCodeByte());
+}
 // 0374  ASSIGN_MOP(z, LGAW)
+void E_LGAW(Opcode*) {
+	E_LGA_(GetCodeWord());
+}
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+__attribute__((always_inline)) static inline void E_DESC_(CARD16 arg) {
+	if (DEBUG_TRACE_RUN) logger.debug("TRACE %6o  DESC %02X", savedPC, arg);
+	Push(GFI | 0x0003);
+	Push(arg);
+}
 // 0375  ASSIGN_MOP(z, DESC)
+void E_DESC(Opcode*) {
+	E_DESC_(GetCodeWord());
+}
 // 0376
 // 0377  ASSIGN_MOP(z, RESRVD)
+void E_RESRVD(Opcode*) {
+	logger.fatal("TRACE %6o  RESRVD", savedPC);
+	ERROR();
+}
