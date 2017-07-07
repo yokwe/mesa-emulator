6767123970854955bc819969bfba72262757c09f
yokwe <hasegawa.yasuhiro+github@gmail.com>
2017-07-07 16:55:09 +0900

    Temporary commit.

---
+++ guam/src/bcd/SERecord.h
@@ -0,0 +1,287 @@
+/*
+Copyright (c) 2017, Yasuhiro Hasegawa
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+  3. The name of the author may not be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+
+//
+// SERecord.h
+//
+
+#ifndef SERECORD_H__
+#define SERECORD_H__
+
+#include "../util/Util.h"
+#include "../mesa/MesaBasic.h"
+
+#include "BCD.h"
+
+#include "Symbols.h"
+
+class SERecord;
+class MDIndex;
+
+//SEIndex: TYPE = Base RELATIVE POINTER [0..Limit) TO SERecord;
+//SENull: SEIndex = FIRST[SEIndex];
+
+class SEIndex {
+public:
+	static const CARD16 SE_NULL = 0;
+	static void resolve();
+
+	Symbols*   symbols;
+	CARD16     index;
+	SERecord* value;
+
+	SEIndex() : symbols(0), index(SE_NULL), value(0) {}
+	SEIndex(Symbols* symbols, CARD16 index);
+
+	QString toString();
+
+private:
+	static QList<SEIndex*> all;
+};
+
+
+//SERecord: TYPE = RECORD [
+//  mark3(0:0..0), mark4(0:1..1): BOOLEAN,
+//  body(0:2..95): SELECT seTag(0:2..2): * FROM
+//    id => [
+//      extended(0:3..3): BOOLEAN,
+//      public(0:4..4): BOOLEAN,
+//      idCtx(0:5..15): CTXIndex,
+//      immutable(1:0..0), constant(1:1..1): BOOLEAN,
+//      idType(1:2..15): SEIndex,
+//      idInfo(2:0..15): Unspec,
+//      idValue(3:0..15): Unspec,
+//      hash(4:0..12): HTIndex,
+//      linkSpace(4:13..13): BOOLEAN,
+//      ctxLink(4:14..31): SELECT linkTag(4:14..15): * FROM
+//        terminal => [],
+//        sequential => [],
+//        linked => [link(5:0..15): ISEIndex]
+//        ENDCASE],
+//    cons => [
+//      typeInfo(0:3..63): SELECT typeTag(0:3..7): TypeClass FROM
+//        mode => [],
+//        basic => [
+//          ordered(0:8..8): BOOLEAN,
+//          code(0:9..15): [0..16),
+//          length(1:0..15): BitLength],
+//        enumerated => [
+//          ordered(0:8..8), machineDep(0:9..9): BOOLEAN,
+//          unpainted(0:10..10): BOOLEAN,     -- un- for backward compatiblity
+//          sparse(0:11..15): BOOLEAN,
+//          valueCtx(1:0..15): CTXIndex,
+//          nValues(2:0..15): CARDINAL],
+//        record => [
+//          hints(0:8..15): RECORD [
+//            comparable(0:0..0), assignable(0:1..1): BOOLEAN,
+//            unifield(0:2..2), variant(0:3..3), privateFields(0:4..4): BOOLEAN,
+//            refField(0:5..5), default(0:6..6), voidable(0:7..7): BOOLEAN],
+//          length(1:0..15): BitLength,
+//          argument(2:0..0), monitored(2:1..1), machineDep(2:2..2): BOOLEAN,
+//          painted(2:3..3): BOOLEAN,
+//          fieldCtx(2:4..14): CTXIndex,
+//          linkPart(2:15..31): SELECT linkTag(2:15..15): * FROM
+//            notLinked => [],
+//            linked => [linkType(3:0..15): SEIndex]
+//            ENDCASE],
+//        ref => [
+//          counted(0:8..8), ordered(0:9..9), readOnly(0:10..10), list(0:11..11), var(0:12..12), basing(0:13..15): BOOLEAN,
+//          refType(1:0..15): SEIndex],
+//        array => [
+//          packed(0:8..15): BOOLEAN,
+//          indexType(1:0..15): SEIndex,
+//          componentType(2:0..15): SEIndex],
+//        arraydesc => [
+//          var(0:8..8), readOnly(0::9..15): BOOLEAN,
+//          describedType(1:0..15): SEIndex],
+//        transfer => [
+//          safe(0:8..8): BOOLEAN,
+//          mode(0:9..15): TransferMode,
+//          typeIn(1:0..15), typeOut(2:0..15): CSEIndex],
+//        definition => [
+//          named(0:8..15): BOOLEAN,
+//          defCtx(1:0..15): CTXIndex],
+//        union => [
+//          hints(0:8..11): RECORD [
+//            equalLengths(0:0..0): BOOLEAN,
+//            refField(0:1..1), default(0:2..2), voidable(0:3..3): BOOLEAN],
+//          overlaid(0:12..12), controlled(0:13..13), machineDep(0:14..15): BOOLEAN,
+//          caseCtx(1:0..15): CTXIndex,
+//          tagSei(2:0..15): ISEIndex],
+//        sequence => [
+//          packed(0:8..8): BOOLEAN,
+//          controlled(0:9..9), machineDep(0:10..15): BOOLEAN,
+//          tagSei(1:0..15): ISEIndex,
+//          componentType(2:0..15): SEIndex],
+//        relative => [
+//          baseType(1:0..15): SEIndex,
+//          offsetType(2:0..15): SEIndex,
+//          resultType(3:0..15): SEIndex],
+//        subrange => [
+//          filled(0:8..8), empty(0:9..15): BOOLEAN,
+//          rangeType(1:0..15): SEIndex,
+//          origin(2:0..15): INTEGER,
+//          range(3:0..15): CARDINAL],
+//        long, real => [rangeType(1:0..15): SEIndex],
+//        opaque => [
+//          lengthKnown(0:8..15): BOOLEAN,
+//          length(1:0..15): BitLength,
+//          id(2:0..15): ISEIndex],
+//        zone => [counted(0:8..8), mds(0:9..15): BOOLEAN],
+//        any => [],
+//        nil => [],
+//        bits => [length(1:0..31): BitCount],   -- placed here to avoid
+//        ENDCASE],         -- changing symbol version id's
+//    ENDCASE];
+class SERecord {
+public:
+	enum class SeTag {
+		ID, CONS,
+	};
+	QString toString(SeTag value);
+
+	enum class LinkTag {
+		TERMINAL, SEQUENTIAL, LINKED,
+	};
+	QString toString(LinkTag value);
+
+	CARD16 index;
+	SeTag seTag;
+
+	union {
+		struct {
+			bool      extended;
+			bool      isPublic;
+			CTXIndex* idCtx;
+			bool      immutable;
+			bool      constant;
+			SEIndex*  idType;
+			CARD16    idInfo;
+			CARD16    idValue;
+			HTIndex*  hash;
+			bool      linkSpace;
+			LinkTag   linkTag;
+			union {
+				struct {
+					SEIndex* link;
+				} linked;
+			};
+		} id;
+		struct {
+			//    cons => [
+			//      typeInfo(0:3..63): SELECT typeTag(0:3..7): TypeClass FROM
+			Symbols::TypeClass typeTag;
+			//        mode => [],
+			//        basic => [
+			//          ordered(0:8..8): BOOLEAN,
+			//          code(0:9..15): [0..16),
+			//          length(1:0..15): BitLength],
+			//        enumerated => [
+			//          ordered(0:8..8), machineDep(0:9..9): BOOLEAN,
+			//          unpainted(0:10..10): BOOLEAN,     -- un- for backward compatiblity
+			//          sparse(0:11..15): BOOLEAN,
+			//          valueCtx(1:0..15): CTXIndex,
+			//          nValues(2:0..15): CARDINAL],
+			//        record => [
+			//          hints(0:8..15): RECORD [
+			//            comparable(0:0..0), assignable(0:1..1): BOOLEAN,
+			//            unifield(0:2..2), variant(0:3..3), privateFields(0:4..4): BOOLEAN,
+			//            refField(0:5..5), default(0:6..6), voidable(0:7..7): BOOLEAN],
+			//          length(1:0..15): BitLength,
+			//          argument(2:0..0), monitored(2:1..1), machineDep(2:2..2): BOOLEAN,
+			//          painted(2:3..3): BOOLEAN,
+			//          fieldCtx(2:4..14): CTXIndex,
+			//          linkPart(2:15..31): SELECT linkTag(2:15..15): * FROM
+			//            notLinked => [],
+			//            linked => [linkType(3:0..15): SEIndex]
+			//            ENDCASE],
+			//        ref => [
+			//          counted(0:8..8), ordered(0:9..9), readOnly(0:10..10), list(0:11..11), var(0:12..12), basing(0:13..15): BOOLEAN,
+			//          refType(1:0..15): SEIndex],
+			//        array => [
+			//          packed(0:8..15): BOOLEAN,
+			//          indexType(1:0..15): SEIndex,
+			//          componentType(2:0..15): SEIndex],
+			//        arraydesc => [
+			//          var(0:8..8), readOnly(0::9..15): BOOLEAN,
+			//          describedType(1:0..15): SEIndex],
+			//        transfer => [
+			//          safe(0:8..8): BOOLEAN,
+			//          mode(0:9..15): TransferMode,
+			//          typeIn(1:0..15), typeOut(2:0..15): CSEIndex],
+			//        definition => [
+			//          named(0:8..15): BOOLEAN,
+			//          defCtx(1:0..15): CTXIndex],
+			//        union => [
+			//          hints(0:8..11): RECORD [
+			//            equalLengths(0:0..0): BOOLEAN,
+			//            refField(0:1..1), default(0:2..2), voidable(0:3..3): BOOLEAN],
+			//          overlaid(0:12..12), controlled(0:13..13), machineDep(0:14..15): BOOLEAN,
+			//          caseCtx(1:0..15): CTXIndex,
+			//          tagSei(2:0..15): ISEIndex],
+			//        sequence => [
+			//          packed(0:8..8): BOOLEAN,
+			//          controlled(0:9..9), machineDep(0:10..15): BOOLEAN,
+			//          tagSei(1:0..15): ISEIndex,
+			//          componentType(2:0..15): SEIndex],
+			//        relative => [
+			//          baseType(1:0..15): SEIndex,
+			//          offsetType(2:0..15): SEIndex,
+			//          resultType(3:0..15): SEIndex],
+			//        subrange => [
+			//          filled(0:8..8), empty(0:9..15): BOOLEAN,
+			//          rangeType(1:0..15): SEIndex,
+			//          origin(2:0..15): INTEGER,
+			//          range(3:0..15): CARDINAL],
+			//        long, real => [rangeType(1:0..15): SEIndex],
+			//        opaque => [
+			//          lengthKnown(0:8..15): BOOLEAN,
+			//          length(1:0..15): BitLength,
+			//          id(2:0..15): ISEIndex],
+			//        zone => [counted(0:8..8), mds(0:9..15): BOOLEAN],
+			//        any => [],
+			//        nil => [],
+			//        bits => [length(1:0..31): BitCount],   -- placed here to avoid
+			//        ENDCASE],         -- changing symbol version id's
+			SEIndex* chain;
+			Closure   copied;
+			MDIndex*  module;
+			SEIndex* map;
+			bool      closed;
+			bool      complete;
+			bool      restricted;
+			bool      reset;
+		} cons;
+	};
+
+	SERecord(Symbols* symbols, CARD16 index);
+
+	QString toString();
+};
+
+
+#endif
