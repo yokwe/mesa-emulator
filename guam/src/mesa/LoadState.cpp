40776af80357cfb245b1a3c9cf0fd8d0cc245488
hasegawa <hasegawa@ubuntu1804.lan>
2018-06-12 15:28:20 +0900

    Build gfiInfoMap as expected.

--- guam/src/mesa/LoadState.cpp
+++ guam/src/mesa/LoadState.cpp
@@ -39,21 +39,95 @@ static log4cpp::Category& logger = Logger::getLogger("loadState");
 #include "../symbols/BCDFile.h"
 #include "../symbols/Symbols.h"
 
-class ModuleInfo;
-class BCDInfo;
 
-class ModuleInfo {
+class ModuleEntry {
 public:
-	const QString   name;
-	const FTRecord* code;
-	const FTRecord* symbol;
+	const quint64 version;
+	const QString moduleName;
 
-	const QMap<CARD16, CARD16>  entryMap;     // key is entryIndex, value is pc
-	const QMap<CARD16, QString> entryNameMap; // key is pc
+	QVector<QString> entryName;
 
-	ModuleInfo(QString name_, FTRecord* code_, FTRecord* symbol_, QMap<CARD16, QString> entryNameMap_) :
-		name(name_), code(code_), symbol(symbol_), entryNameMap(entryNameMap_) {}
+	ModuleEntry(quint64 version_, QString moduleName_) : version(version_), moduleName(moduleName_) {}
+
+	QString toString() {
+		return QString("%1 %2 %3").arg(version).arg(moduleName).arg(entryName.size());
+	}
 };
+static QMap<quint64, ModuleEntry*> moduleEntryMap; // key is module file version
+
+static void initializeModuleEntryMap() {
+	QString PATH_MODULE_ENTRY("tmp/moduleEntry/");
+	QDirIterator i(PATH_MODULE_ENTRY);
+
+	while(i.hasNext()) {
+		QString path = i.next();
+//		logger.info("path = %s", path.toLocal8Bit().constData());
+
+		QFileInfo fileInfo(path);
+		if (fileInfo.isDir()) continue;
+
+		QFile file(path);
+		{
+			bool ok = file.open(QIODevice::ReadOnly | QIODevice::Text);
+			if (!ok) {
+				logger.fatal("path = %s", path.toLocal8Bit().constData());
+				logger.fatal("Unexpected errorString = %s", file.errorString().toLocal8Bit().constData());
+				ERROR();
+			}
+		}
+		QTextStream text(&file);
+		uint count = 0;
+		ModuleEntry* entry = 0;
+		for(;;) {
+			if (text.atEnd()) break;
+			QString line = text.readLine();
+			if (line.isNull()) break;
+
+			QStringList token = line.split(' ');
+			if (token.size() != 4) {
+				logger.fatal("path = %s", path.toLocal8Bit().constData());
+				logger.fatal("Unexpected line = %s", line.toLocal8Bit().constData());
+				ERROR();
+			}
+
+			bool ok;
+			const quint64 version     = token.at(0).toULongLong(&ok);
+			if (!ok) {
+				logger.fatal("path = %s", path.toLocal8Bit().constData());
+				logger.fatal("Unexpected line = %s", line.toLocal8Bit().constData());
+				logger.fatal("version = %s!", token.at(0).toLocal8Bit().constData());
+				ERROR();
+			}
+			const QString moduleName  = token.at(1);
+			const uint    entryIndex  = token.at(2).toUInt(&ok);
+			if (!ok) {
+				logger.fatal("path = %s", path.toLocal8Bit().constData());
+				logger.fatal("Unexpected line = %s", line.toLocal8Bit().constData());
+				logger.fatal("entryIndex = %s!", token.at(2).toLocal8Bit().constData());
+				ERROR();
+			}
+			const QString entryName   = token.at(3);
+
+			if (count == 0) {
+				entry = new ModuleEntry(version, moduleName);
+			}
+			if (count != entryIndex) {
+				logger.fatal("path = %s", path.toLocal8Bit().constData());
+				logger.fatal("Unexpected line = %s", line.toLocal8Bit().constData());
+				logger.fatal("entryIndex = %s!", token.at(2).toLocal8Bit().constData());
+				logger.fatal("count = %d", count);
+				ERROR();
+			}
+			entry->entryName.append(entryName);
+
+			//
+			count++;
+		}
+
+		moduleEntryMap.insert(entry->version, entry);
+	}
+}
+
 
 class BCDModuleInfo {
 public:
@@ -68,8 +142,35 @@ public:
     bool operator<(const BCDModuleInfo& that) const {
     	return (this->bcdIndex != that.bcdIndex) ? (this->bcdIndex < that.bcdIndex) : (this->moduleIndex < that.moduleIndex);
     }
+
+    QString toString() {
+    	return QString("%1-%2").arg(bcdIndex).arg(moduleIndex);
+    }
 };
-static QMap<BCDModuleInfo, CARD32>codebaseMap;
+static QMap<BCDModuleInfo, CARD16>  gfiMap;    // scanModule add entries
+
+class GFInfo {
+public:
+	CARD16  gfi;
+	QString name;
+
+	QString fileName;
+	quint64 fileVersion;
+
+	CARD32  codebase;
+	bool    dummyEntryName;
+
+	QMap<CARD16, QString> entryNameMap; // key = initialPC, value = name
+
+	GFInfo(CARD16 gfi_, QString name_, QString fileName_, quint64 fileVersion_, CARD32 codebase_) :
+		gfi(gfi_), name(name_), fileName(fileName_), fileVersion(fileVersion_), codebase(codebase_), dummyEntryName(0) {}
+
+	QString toString() {
+		return QString("%1 %2 %3 %4 %5 %6 %7").arg(gfi, 4, 16).arg(name, -32).arg(fileName, -32).arg(fileVersion).arg(codebase, 8, 16).arg(dummyEntryName ? "D" : " ").arg(entryNameMap.size(), 3);
+	}
+};
+static QMap<BCDModuleInfo, GFInfo*> gfInfoMap; // scanBCD add entries
+
 
 static void scanBCD(CARD32 loadStateAddress, LoadStateFormat::Object& loadState) {
 	static QSet<CARD16> done;
@@ -106,51 +207,80 @@ static void scanBCD(CARD32 loadStateAddress, LoadStateFormat::Object& loadState)
 			ERROR();
 		}
 
-		done.insert(bcdInfo.id);
-
 		QList<MTRecord*> mtList = bcd->mt.values();
 
-		CARD16 moduleIndex = 0;
+		CARD16 moduleIndex = 1; // moduleIndex starts from 1
 		for(MTRecord* mt: mtList) {
 			if (mt->isNull()) continue;
 
 			const QString   name(mt->name);
 			const FTRecord* file(mt->file);
-			const FTRecord* code(mt->code->sgi->file);
-			const FTRecord* symbol(mt->sseg->file);
 
-			if (!code->isSelf()) {
-				logger.fatal("Unexpected code is not self");
-				ERROR();
-			}
-
-			QMap<CARD16, CARD16>  entryMap;     // key is entryIndex, value is pc
-			CARD16 entryIndex = 0;
-			for(CARD16 initialPC: mt->entries->initialPC) {
-				entryMap[entryIndex++] = initialPC + 1; // plus one to skip fsi
-			}
-
+			GFInfo* gfInfo = 0;
 			BCDModuleInfo key(bcdIndex, moduleIndex);
-			CARD32 codebase = codebaseMap[key];
-
-			logger.info("MODULE %5d %5d %8X %-24s %-48s %s",
-					bcdIndex, moduleIndex, codebase,
-					name.toLocal8Bit().constData(),
-					file->toString().toLocal8Bit().constData(),
-					mt->entries->toString().toLocal8Bit().constData());
-
-			if (symbol->isNull()) {
-				// No output
-			} else if (symbol->isSelf()) {
-				// If symbol is SELF, use mt->file as instead.
-				logger.info("SYMBOL %s %-24s", mt->file->version->toString().toLocal8Bit().constData(), mt->name.toLocal8Bit().constData());
+			if (gfInfoMap.contains(key)) {
+				gfInfo = gfInfoMap[key];
+				logger.fatal("Duplicate gfInfo = %s", gfInfo->toString().toLocal8Bit().constData());
+				ERROR();
 			} else {
-				logger.info("SYMBOL %s %-24s %s %-24s", mt->file->version->toString().toLocal8Bit().constData(), mt->name.toLocal8Bit().constData(), symbol->version->toString().toLocal8Bit().constData(), symbol->name.toLocal8Bit().constData());
+				if (gfiMap.contains(key)) {
+					CARD16  gfi         = gfiMap[key];
+					QString fileName    = file->name;
+					quint64 fileVersion = file->version->value;
+					CARD32  codebase    = ReadDbl(GFT_OFFSET(gfi, codebase)) & ~1;
+
+					gfInfo = new GFInfo(gfi, name, file->name, file->version->value, codebase);
+					gfInfoMap[key] = gfInfo;
+
+					int initialPCSize = mt->entries->initialPC.size();
+					if (initialPCSize != 0 && moduleEntryMap.contains(fileVersion)) {
+						ModuleEntry* moduleEntry = moduleEntryMap[fileVersion];
+						int moduleEntrySize = moduleEntry->entryName.size();
+						if (moduleEntrySize == initialPCSize) {
+							for(int i = 0; i < initialPCSize; i++) {
+								CARD16  initialPC = mt->entries->initialPC[i];
+								QString entryName = moduleEntry->entryName[i];
+								QString name = QString("%1.%2").arg(fileName).arg(entryName);
+								gfInfo->entryNameMap[initialPC] = name;
+							}
+						} else {
+							logger.fatal("Unexpected  entrySize");
+							logger.fatal("entrySize  initialPCSize = %d   moduleEntry = %d", initialPCSize, moduleEntrySize);
+							logger.fatal("mt %s", mt->toString().toLocal8Bit().constData());
+							logger.fatal("moduleEntry %s", moduleEntry->toString().toLocal8Bit().constData());
+							logger.fatal("gfInfo %s", gfInfo->toString().toLocal8Bit().constData());
+							ERROR();
+						}
+					} else {
+						// Add dummy entry to entryName
+						for(int i = 0; i < initialPCSize; i++) {
+							CARD16  initialPC = mt->entries->initialPC[i];
+							QString entryName = QString("%1").arg(i);
+							QString name = QString("%1.%2").arg(fileName).arg(entryName);
+							gfInfo->entryNameMap[initialPC] = name;
+						}
+						gfInfo->dummyEntryName = 1;
+					}
+					logger.info("gfInfo %s", gfInfo->toString().toLocal8Bit().constData());
+				} else {
+					logger.fatal("Unexpected key = %s", key.toString().toLocal8Bit().constData());
+					ERROR();
+				}
 			}
+
+			logger.info("MODULE %5d %5d %s",
+					bcdIndex, moduleIndex, gfInfo->toString().toLocal8Bit().constData());
+
+			logger.info("SYMBOL %s %-24s", mt->file->version->toString().toLocal8Bit().constData(), mt->name.toLocal8Bit().constData());
 			moduleIndex++;
 		}
+
+		//
+		done.insert(bcdInfo.id);
 	}
 }
+
+
 
 static void scanModule(CARD32 loadStateAddress, LoadStateFormat::Object& loadState) {
 	static QSet<CARD16> done;
@@ -166,15 +296,15 @@ static void scanModule(CARD32 loadStateAddress, LoadStateFormat::Object& loadSta
 		if (done.contains(moduleInfo.globalFrame)) continue;
 		done.insert(moduleInfo.globalFrame);
 
-		CARD32 codebase    = ReadDbl(GFT_OFFSET(moduleInfo.globalFrame, codebase)) & ~1;
 		CARD16 bcdIndex    = moduleInfo.index;
 		CARD16 moduleIndex = moduleInfo.cgfi;
+		CARD16 gfi         = moduleInfo.globalFrame;
 
 		BCDModuleInfo key(bcdIndex, moduleIndex);
-		codebaseMap[key] = codebase;
+		gfiMap[key] = moduleInfo.globalFrame;
 
 //		logger.info("module %4d %d %5d %5d %4X  %8X", i, moduleInfo.resolved, moduleInfo.cgfi, moduleInfo.index, moduleInfo.globalFrame, codebase);
-		logger.info("module %5d %5d %8X", bcdIndex, moduleIndex, codebase);
+		logger.info("module %5d %5d %4X", bcdIndex, moduleIndex, gfi);
 	}
 }
 
@@ -201,6 +331,11 @@ static void scan(CARD32 loadStateAddress) {
 
 void scanLoadState() {
 	try {
+		if (moduleEntryMap.size() == 0) {
+			initializeModuleEntryMap();
+			logger.info("moduleEntryMap %d", moduleEntryMap.size());
+		}
+
 		// Sanity check
 		CARD32 pesv = ReadDbl(CPSwapDefs::PSEV);
 		if (pesv == 0) return;
