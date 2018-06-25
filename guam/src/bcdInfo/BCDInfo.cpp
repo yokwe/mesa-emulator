854b43689d5ed78e77c0b41b98488370fdde2c84
hasegawa <hasegawa@ubuntu1804.lan>
2018-06-26 08:54:11 +0900

    Temporary commit(Work in progress).

--- guam/src/bcdInfo/BCDInfo.cpp
+++ guam/src/bcdInfo/BCDInfo.cpp
@@ -128,6 +128,37 @@ void JSONBase::getJsonValue(const QJsonObject& jsonObject, const QString& key, b
 		ERROR();
 	}
 }
+void JSONBase::getJsonValue(const QJsonObject& jsonObject, const QString& key, QJsonObject&  value) {
+	if (jsonObject.contains(key)) {
+		QJsonValue j = jsonObject.value(key);
+		if (j.isObject()) {
+			value = j.toObject();
+		} else {
+			logger.fatal("Unexpected key = %s type = %d", key.toLocal8Bit().constData(), j.type());
+			logger.fatal("Expected type = Object");
+			ERROR();
+		}
+	} else {
+		logger.fatal("Key doesn't exist. key = %s", key.toLocal8Bit().constData());
+		ERROR();
+	}
+}
+void JSONBase::getJsonValue(const QJsonObject& jsonObject, const QString& key, QJsonArray&  value) {
+	if (jsonObject.contains(key)) {
+		QJsonValue j = jsonObject.value(key);
+		if (j.isArray()) {
+			value = j.toArray();
+		} else {
+			logger.fatal("Unexpected key = %s type = %d", key.toLocal8Bit().constData(), j.type());
+			logger.fatal("Expected type = Array");
+			ERROR();
+		}
+	} else {
+		logger.fatal("Key doesn't exist. key = %s", key.toLocal8Bit().constData());
+		ERROR();
+	}
+}
+
 
 
 void JSONBase::setJsonValue(QJsonObject& jsonObject, const QString& key, const QString& value) {
@@ -158,7 +189,144 @@ void JSONBase::setJsonValue(QJsonObject& jsonObject, const QString& key, const b
 		jsonObject[key] = QJsonValue::fromVariant(QVariant(value));
 	}
 }
+void JSONBase::setJsonValue(QJsonObject& jsonObject, const QString& key, const QJsonObject& value) {
+	if (jsonObject.contains(key)) {
+		logger.fatal("Duplicate key = %s", key.toLocal8Bit().constData());
+	} else {
+		jsonObject[key] = value;
+	}
+}
+void JSONBase::setJsonValue(QJsonObject& jsonObject, const QString& key, const QJsonArray& value) {
+	if (jsonObject.contains(key)) {
+		logger.fatal("Duplicate key = %s", key.toLocal8Bit().constData());
+	} else {
+		jsonObject[key] = value;
+	}
+}
 
+
+//
+// FTInfo
+//
+FTInfo::FTInfo() {
+	this->name    = "#NULL#";
+	this->version = 0;
+}
+FTInfo::FTInfo(const FTRecord& ft) {
+	if (ft.isNull()) {
+		this->name    = "#NULL#";
+		this->version = VERSION_NULL;
+	} else if (ft.isSelf()) {
+		this->name    = "#SELF#";
+		this->version = VERSION_SELF;
+	} else {
+		this->name    = ft.name;
+		this->version = ft.version->value;
+	}
+}
+void FTInfo::getJsonValue(const QJsonObject& json) {
+	GET_JSON_FIELD(json, name);
+	GET_JSON_FIELD(json, version);
+}
+void FTInfo::setJsonValue(QJsonObject& json) const {
+	SET_JSON_FIELD(json, name);
+	SET_JSON_FIELD(json, version);
+}
+
+void FTInfo::getJsonValue(const QJsonObject& json, const QString& key, FTInfo&  value) {
+	QJsonObject jo;
+	JSONBase::getJsonValue(json, key, jo);
+	value.getJsonValue(jo);
+}
+void FTInfo::setJsonValue(QJsonObject& json, const QString& key, const FTInfo&  value) {
+	QJsonObject jo;
+	value.setJsonValue(jo);
+	JSONBase::setJsonValue(json, key, jo);
+}
+void FTInfo::getJsonArray(const QJsonArray& ja, QList<FTInfo>& list) {
+	for(const QJsonValue& e: ja) {
+		if (e.isObject()) {
+			QJsonObject o = e.toObject();
+			FTInfo ft(o);
+			list.append(ft);
+		} else {
+			logger.fatal("Unexpected type = %d", e.type());
+			logger.fatal("Expected type = Object");
+			ERROR();
+		}
+	}
+}
+void FTInfo::setJsonArray(QJsonArray& ja, const QList<FTInfo>& list) {
+	for(const FTInfo& e: list) {
+		QJsonObject jo;
+		e.setJsonValue(jo);
+		ja.append(jo);
+	}
+}
+
+
+//
+// SGInfo
+//
+SGInfo::SGInfo() {
+	this->file       = FTInfo();
+	this->base       = 0;
+	this->pages      = 0;
+	this->extraPages = 0;
+	this->segClass   = "Undefined";
+}
+SGInfo::SGInfo(const SGRecord& that) {
+	this->file       = FTInfo(*that.file);
+	this->base       = that.base;
+	this->pages      = that.pages;
+	this->extraPages = that.extraPages;
+	this->segClass   = SGRecord::toString(that.segClass);
+}
+void SGInfo::getJsonValue(const QJsonObject& json) {
+	FTInfo::getJsonValue(json, "file", file);
+	GET_JSON_FIELD(json, base);
+	GET_JSON_FIELD(json, pages);
+	GET_JSON_FIELD(json, extraPages);
+	GET_JSON_FIELD(json, segClass);
+}
+void SGInfo::setJsonValue(QJsonObject& json) const {
+	FTInfo::setJsonValue(json, "file", file);
+	SET_JSON_FIELD(json, base);
+	SET_JSON_FIELD(json, pages);
+	SET_JSON_FIELD(json, extraPages);
+	SET_JSON_FIELD(json, segClass);
+}
+
+void SGInfo::getJsonValue(const QJsonObject& json, const QString& key, SGInfo&  value) {
+	QJsonObject jo;
+	JSONBase::getJsonValue(json, key, jo);
+	value.getJsonValue(jo);
+}
+void SGInfo::setJsonValue(QJsonObject& json, const QString& key, const SGInfo&  value) {
+	QJsonObject jo;
+	value.setJsonValue(jo);
+	JSONBase::setJsonValue(json, key, jo);
+}
+void SGInfo::getJsonArray(const QJsonArray& ja, QList<SGInfo>& list) {
+	for(const QJsonValue& e: ja) {
+		if (e.isObject()) {
+			QJsonObject o = e.toObject();
+			SGInfo ft(o);
+			list.append(ft);
+		} else {
+			logger.fatal("Unexpected type = %d", e.type());
+			logger.fatal("Expected type = Object");
+			ERROR();
+		}
+	}
+}
+void SGInfo::setJsonArray(QJsonArray& ja, const QList<SGInfo>& list) {
+	for(const SGInfo& e: list) {
+		QJsonObject jo;
+		e.setJsonValue(jo);
+		ja.append(jo);
+	}
+}
 
 //
 // BCDInfo
@@ -166,7 +334,28 @@ void JSONBase::setJsonValue(QJsonObject& jsonObject, const QString& key, const b
 BCDInfo::BCDInfo(BCD& bcd) {
 	path        = bcd.getPath();
 	version     = bcd.version->value;
-	definitions = bcd.definitions;
+
+	sourceFile     = FTInfo(*bcd.sourceFile);
+	unpackagedFile = FTInfo(*bcd.unpackagedFile);
+
+	nConfigs = bcd.nConfigs;
+	nModules = bcd.nModules;
+	nImports = bcd.nImports;
+	nExports = bcd.nExports;
+	nPages   = bcd.nPages;
+
+	definitions   = bcd.definitions;
+	repackaged    = bcd.repackaged;
+	typeExported  = bcd.typeExported;
+	tableCompiled = bcd.tableCompiled;
+
+	for(SGRecord* e: bcd.sg.values()) {
+		// Skip null entry
+		if (e->isNull()) continue;
+
+		SGInfo sg(*e);
+		sgList.append(sg);
+	}
 }
 BCDInfo::BCDInfo(QJsonObject& json) {
 	getJsonValue(json);
@@ -176,11 +365,45 @@ BCDInfo::BCDInfo(QJsonObject& json) {
 void BCDInfo::getJsonValue(const QJsonObject& json) {
 	GET_JSON_FIELD(json, path);
 	GET_JSON_FIELD(json, version);
+
+	FTInfo::getJsonValue(json, "sourceFile",     sourceFile);
+	FTInfo::getJsonValue(json, "unpackagedFile", unpackagedFile);
+
+	GET_JSON_FIELD(json, nConfigs);
+	GET_JSON_FIELD(json, nModules);
+	GET_JSON_FIELD(json, nImports);
+	GET_JSON_FIELD(json, nExports);
+	GET_JSON_FIELD(json, nPages);
+
 	GET_JSON_FIELD(json, definitions);
+	GET_JSON_FIELD(json, repackaged);
+	GET_JSON_FIELD(json, typeExported);
+	GET_JSON_FIELD(json, tableCompiled);
+
+	QJsonArray ja;
+	SGInfo::getJsonArray(ja, sgList);
+	JSONBase::getJsonValue(json, "sgList", ja);
 }
 // write value to jsonObject
-void BCDInfo::setJsonValue(QJsonObject& json) {
+void BCDInfo::setJsonValue(QJsonObject& json) const {
 	SET_JSON_FIELD(json, path);
 	SET_JSON_FIELD(json, version);
+
+	FTInfo::setJsonValue(json, "sourceFile",     sourceFile);
+	FTInfo::setJsonValue(json, "unpackagedFile", unpackagedFile);
+
+	SET_JSON_FIELD(json, nConfigs);
+	SET_JSON_FIELD(json, nModules);
+	SET_JSON_FIELD(json, nImports);
+	SET_JSON_FIELD(json, nExports);
+	SET_JSON_FIELD(json, nPages);
+
 	SET_JSON_FIELD(json, definitions);
+	SET_JSON_FIELD(json, repackaged);
+	SET_JSON_FIELD(json, typeExported);
+	SET_JSON_FIELD(json, tableCompiled);
+
+	QJsonArray ja;
+	SGInfo::setJsonArray(ja, sgList);
+	JSONBase::setJsonValue(json, "sgList", ja);
 }
--- guam/src/bcdInfo/BCDInfo.h
+++ guam/src/bcdInfo/BCDInfo.h
@@ -40,6 +40,9 @@ OF SUCH DAMAGE.
 #define SET_JSON_FIELD(JSON,FIELD) JSONBase::setJsonValue(JSON, #FIELD, FIELD)
 #define GET_JSON_FIELD(JSON,FIELD) JSONBase::getJsonValue(JSON, #FIELD, FIELD)
 
+class FTInfo;
+class SGInfo;
+
 class JSONBase {
 public:
 	virtual ~JSONBase() {}
@@ -47,34 +50,101 @@ public:
 	// read value from jsonObject
 	virtual void getJsonValue(const QJsonObject& json) = 0;
 	// write value to jsonObject
-	virtual void setJsonValue(QJsonObject& json) = 0;
+	virtual void setJsonValue(QJsonObject& json) const = 0;
 
 	// read value from jsonObject
-	static void getJsonValue(const QJsonObject& json, const QString& key, QString& value);
-	static void getJsonValue(const QJsonObject& json, const QString& key, CARD16&  value);
-	static void getJsonValue(const QJsonObject& json, const QString& key, quint64& value);
-	static void getJsonValue(const QJsonObject& json, const QString& key, bool&    value);
+	static void getJsonValue(const QJsonObject& json, const QString& key, QString&     value);
+	static void getJsonValue(const QJsonObject& json, const QString& key, CARD16&      value);
+	static void getJsonValue(const QJsonObject& json, const QString& key, quint64&     value);
+	static void getJsonValue(const QJsonObject& json, const QString& key, bool&        value);
+	static void getJsonValue(const QJsonObject& json, const QString& key, QJsonObject& value);
+	static void getJsonValue(const QJsonObject& json, const QString& key, QJsonArray&  value);
 
 	// write value to jsonObject
-	static void setJsonValue(QJsonObject& json, const QString& key, const QString& value);
-	static void setJsonValue(QJsonObject& json, const QString& key, const CARD16&  value);
-	static void setJsonValue(QJsonObject& json, const QString& key, const quint64& value);
-	static void setJsonValue(QJsonObject& json, const QString& key, const bool&    value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const QString&     value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const CARD16&      value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const quint64&     value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const bool&        value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const FTInfo&      value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const SGInfo&      value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const QJsonObject& value);
+	static void setJsonValue(QJsonObject& json, const QString& key, const QJsonArray&  value);
 };
 
 class FTInfo : public JSONBase {
 public:
+	static const quint64 VERSION_NULL = 0;
+	static const quint64 VERSION_SELF = 1;
+
+	static void getJsonValue(const QJsonObject& json, const QString& key, FTInfo&        value);
+	static void setJsonValue(QJsonObject& json,       const QString& key, const FTInfo&  value);
+
+	static void setJsonArray(QJsonArray& ja,       const QList<FTInfo>& list);
+	static void getJsonArray(const QJsonArray& ja, QList<FTInfo>&       list);
+
+
 	QString name;
 	quint64 version;
 
-	FTInfo(FTRecord& ftRecoed);
-	FTInfo(QJsonObject& jsonObject);
+	FTInfo();
+	FTInfo(const FTRecord& that);
+	FTInfo(const QJsonObject& jsonObject) {
+		getJsonValue(jsonObject);
+	}
+	FTInfo(const FTInfo& that) {
+		this->name    = that.name;
+		this->version = that.version;
+	}
 	virtual ~FTInfo() {}
 
 	// read value from jsonObject
 	void getJsonValue(const QJsonObject& json);
 	// write value to jsonObject
-	void setJsonValue(QJsonObject& json);
+	void setJsonValue(QJsonObject& json) const;
+
+	bool isNull() const {
+		return version == VERSION_NULL;
+	}
+	bool isSelf() const {
+		return version == VERSION_SELF;
+	}
+};
+
+class SGInfo : public JSONBase {
+public:
+	typedef SGRecord::SegClass SegClass;
+
+	static void getJsonValue(const QJsonObject& json, const QString& key, SGInfo&        value);
+	static void setJsonValue(QJsonObject& json,       const QString& key, const SGInfo&  value);
+
+	static void setJsonArray(QJsonArray& ja,       const QList<SGInfo>& list);
+	static void getJsonArray(const QJsonArray& ja, QList<SGInfo>&       list);
+
+	FTInfo  file;
+	CARD16  base;
+	CARD16  pages;
+	CARD16  extraPages;
+	QString segClass;
+
+	SGInfo();
+	SGInfo(const SGRecord& that);
+	SGInfo(const QJsonObject& jsonObject) {
+		getJsonValue(jsonObject);
+	}
+	SGInfo(const SGInfo& that) {
+		this->file       = that.file;
+		this->base       = that.base;
+		this->pages      = that.pages;
+		this->extraPages = that.extraPages;
+		this->segClass   = that.segClass;
+	}
+
+	virtual ~SGInfo() {}
+
+	// read value from jsonObject
+	void getJsonValue(const QJsonObject& json);
+	// write value to jsonObject
+	void setJsonValue(QJsonObject& json) const;
 };
 
 class BCDInfo : public JSONBase {
@@ -83,10 +153,21 @@ public:
 
 	quint64 version;
 
-	bool definitions;
+	FTInfo sourceFile;
+	FTInfo unpackagedFile;
 
-//	FTInfo* sourceFile;
-//	FTInfo* unpackagedFile;
+	CARD16 nConfigs;
+	CARD16 nModules;
+	CARD16 nImports;
+	CARD16 nExports;
+	CARD16 nPages;
+
+	bool   definitions;
+	bool   repackaged;
+	bool   typeExported;
+	bool   tableCompiled;
+
+	QList<SGInfo> sgList; // Treat as QJsonArray
 
 	BCDInfo(BCD& bcd);
 	BCDInfo(QJsonObject& jsonObject);
@@ -95,7 +176,7 @@ public:
 	// read value from jsonObject
 	void getJsonValue(const QJsonObject& json);
 	// write value to jsonObject
-	void setJsonValue(QJsonObject& json);
+	void setJsonValue(QJsonObject& json) const;
 };
 
 #endif
