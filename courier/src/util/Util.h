c0d2b248fe276a7ae77a6227a764a118da66ef3c
hasegawa <hasegawa@ubuntudev.local>
2019-03-13 16:56:07 +0900

    Update copyright year.

--- courier/src/util/Util.h
+++ courier/src/util/Util.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014, Yasuhiro Hasegawa
+Copyright (c) 2019, Yasuhiro Hasegawa
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
