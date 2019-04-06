a6eb6800bf9ae499f59617d135130a25f76f0b2f
hasegawa <hasegawa@ubuntudev.local>
2019-04-06 10:14:41 +0900

    Generate procedure and error list of program.

--- courier/src-java/mesa/app/GenStub.java
+++ courier/src-java/mesa/app/GenStub.java
@@ -1,11 +1,13 @@
 package mesa.app;
 
-import mesa.courier.compiler.Compiler;
-import mesa.courier.program.Program;
-import mesa.courier.program.ProgramBuilder;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import mesa.courier.compiler.Compiler;
+import mesa.courier.program.Program;
+import mesa.courier.program.ProgramBuilder;
 
 public class GenStub {
 	protected static final Logger logger = LoggerFactory.getLogger(GenStub.class);
@@ -25,6 +27,22 @@ public class GenStub {
 			Compiler compiler = new Compiler(program);
 			compiler.genStub();
 		}
+		
+		// TODO generate program version procedure mapping table
+		for(Map.Entry<String, Compiler.Context> entry: Compiler.contextMap.entrySet()) {
+			String           programVersion = entry.getKey();
+			Compiler.Context context        = entry.getValue();
+			
+			if (context.procedureInfoList.isEmpty()) continue;
+						
+			for(Compiler.ProcedureInfo procedureInfo: context.procedureInfoList) {
+				logger.info("{}  PROC  {}  {}", programVersion, procedureInfo.code, procedureInfo.name);
+			}
+
+			for(Compiler.ErrorInfo errorInfo: context.errorInfoList) {
+				logger.info("{}  ERROR {}  {}", programVersion, errorInfo.code, errorInfo.name);
+			}
+}
 		
 		logger.debug("STOP");
 	}
