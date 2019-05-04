d4202d11b893c93c985a3cc73d9655921ba1e5fc
hasegawa <hasegawa@ubuntudev.local>
2019-05-04 10:49:35 +0900

    Reorganize contents.

--- courier/data/courier/custom/SequencedPacket.cr
+++ courier/data/courier/custom/SequencedPacket.cr
@@ -13,43 +13,21 @@ Control: TYPE = MACHINE DEPENDENT [
 ] OF BYTE;
 
 Type: TYPE = {
-    DATA         (0),
-    BULK         (1),
-    CLOSE      (254),
-    CLOSE_REPLY(255)
+    DATA         (0), -- SST used for all Courier messages
+    BULK         (1), -- SST used for bulk data
+    CLOSE      (254), -- SST used for closing connection
+    CLOSE_REPLY(255)  -- SST used for reply of closing connection (handshake)
 } OF BYTE;
 
 Header: TYPE = RECORD [
-    Control:     control,
-    Type:        type,
-    control:     UNSPECIFIED,
-    source:      ConnectionID,
-    destination: ConnectionID,
-    sequence:    SequenceNumber,
-    acknowledge: SequenceNumber,
-    allocation:  SequenceNumber
+    control: Control,
+    type:    Type,
+    src:     ConnectionID,
+    dst:     ConnectionID,
+    seq:     SequenceNumber,
+    ack:     SequenceNumber,
+    alloc:   SequenceNumber
     -- Data is stored after allocation
 ];
-
--- mask for control
-maskSystemPacket:        UNSPECIFIED = 08000X;
-maskSendAcknowledgement: UNSPECIFIED = 04000X;
-maskAttention:           UNSPECIFIED = 02000X;
-maskEndOfMessage:        UNSPECIFIED = 01000X;
-maskDatastreamType:      UNSPECIFIED = 000FFX;
-
--- datastreamType AKA SubSystemType - SST
--- from Courier/Friends/CourierProtocol.mesa
-dataSST:       UNSPECIFIED =   0; --SST used for all Courier messages
--- from Courier/Private/BulkData.mesa
-bulkSST:       UNSPECIFIED =   1; -- SST used for bulk data
--- from NS/Public/NetworkStream.mesa
-closeSST:      UNSPECIFIED = 254; -- SST used for closing connection
-closeReplySST: UNSPECIFIED = 255; -- SST used for rely of closing connection (handshake)
-
-sstCourier:    UNSPECIFIED =   0; -- alias of dataSST
-sstBulk:       UNSPECIFIED =   1; -- alias of bulkSST
-sstClose:      UNSPECIFIED = 254; -- alias of closeSST
-sstCloseReply: UNSPECIFIED = 255; -- alias of closeReplySST
 
 END.
