07dff85da6872c3e3f11f304f88e0ff57d7aaaae
hasegawa <hasegawa@ubuntudev.local>
2019-05-10 09:16:26 +0900

    Use Socket:Manager for courier-app.

--- courier/src/courier-app/courier-app.cpp
+++ courier/src/courier-app/courier-app.cpp
@@ -31,93 +31,31 @@ OF SUCH DAMAGE.
 #include "../util/Util.h"
 static log4cpp::Category& logger = Logger::getLogger("courier-app");
 
-#include "../courier/NIC.h"
-#include "../courier/IDP.h"
-#include "../courier/RIP.h"
-#include "../courier/PEX.h"
-#include "../courier/Time.h"
-#include "../courier/ExpeditedCourier.h"
-#include "../courier/Programs.h"
+#include "../util/NIC.h"
+#include "../courier/Socket.h"
+#include "../stub/Ethernet.h"
 
 int main(int /*argc*/, char** /*argv*/) {
 	logger.info("START");
 
 	setSignalHandler();
 
-	Courier::NIC nic;
-	nic.attach("ens192");
+	const char* nicName = "ens192";
+	Courier::Socket::Network myNetwork = (Courier::Socket::Network)10;
 
-	logger.info("host = %s", Courier::NIC::toStarStyleAddress(nic.getAddress()).toLocal8Bit().constData());
-	nic.discardPacket();
+	logger.info("nicName = %s", nicName);
+	logger.info("myNetwork = %s", Courier::toString(myNetwork).toLocal8Bit().constData());
 
-	Courier::NIC::Data  data;
-	Courier::NIC::Frame ether;
-	Courier::IDP::Frame idp;
+	NIC nic;
+	nic.attach(nicName, (quint16)Courier::Stub::Ethernet::Type::IDP);
+	logger.info("host = %s", Courier::Socket::toStarStyleAddress(nic.getAddress()).toLocal8Bit().constData());
 
-	for(int i = 0; i < 1000; i++) {
-		nic.receive(data.block);
+	Courier::Socket::Manager manager(nic, myNetwork);
 
-		Courier::deserialize(data.block, ether);
-//		logger.info(" data %s", Courier::toString(ether).toLocal8Bit().constData());
+	manager.startService();
+	QThread::sleep(30);
+	manager.stopService();
 
-		Courier::deserialize(ether.data, idp);
-
-		switch(idp.dst.socket) {
-		case Courier::IDP::Socket::RIP:
-		{
-			Courier::RIP::Frame rip;
-			Courier::deserialize(ether.data, rip);
-			logger.info("RIP  %s", Courier::toString(rip).toLocal8Bit().constData());
-		}
-			break;
-		case Courier::IDP::Socket::TIME:
-		{
-			if (idp.packetType == Courier::IDP::PacketType::PEX) {
-				Courier::PEX::Frame pex;
-				Courier::deserialize(ether.data, pex);
-				logger.info("PEX  %s", Courier::toString(pex).toLocal8Bit().constData());
-				Courier::Time::Frame time;
-				Courier::deserialize(pex.data, time);
-				logger.info("TIME %s", Courier::toString(time).toLocal8Bit().constData());
-			} else {
-				logger.info("IDP  %s", Courier::toString(idp).toLocal8Bit().constData());
-			}
-		}
-			break;
-		case Courier::IDP::Socket::CHS:
-		{
-			if (idp.packetType == Courier::IDP::PacketType::PEX) {
-				Courier::PEX::Frame pex;
-				Courier::deserialize(ether.data, pex);
-				logger.info("PEX  %s", Courier::toString(pex).toLocal8Bit().constData());
-
-				Courier::ExpeditedCourier::Frame expedited;
-				Courier::deserialize(pex.data, expedited);
-				logger.info("EXP  %s", Courier::toString(expedited).toLocal8Bit().constData());
-
-				switch(expedited.message.messageType) {
-				case Courier::Protocol::MessageType::call:
-				{
-					Courier::Protocol::Protocol3::CallMessage& callMessage = expedited.message.call();
-					Courier::Programs::Program& program = Courier::Programs::Program::getProgram(callMessage.program);
-					Courier::Programs::Procedure& procedure = Courier::Programs::Program::getProcedure(callMessage.program, callMessage.version, callMessage.procedure);
-					logger.info("     %s%d::%s", program.name.toLocal8Bit().constData(), callMessage.version, procedure.name.toLocal8Bit().constData());
-				}
-					break;
-				default:
-					break;
-				}
-
-			} else {
-				logger.info("IDP  %s", Courier::toString(idp).toLocal8Bit().constData());
-			}
-		}
-			break;
-		default:
-			logger.info("IDP  %s", Courier::toString(idp).toLocal8Bit().constData());
-			break;
-		}
-	}
 	nic.detach();
 
 	logger.info("STOP");
