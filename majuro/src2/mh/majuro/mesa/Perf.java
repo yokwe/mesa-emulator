e799aad5ab48e3b6d02da7c2a73e18875244b641
hasegawa <hasegawa@ubuntu-2004.lan>
2020-05-13 21:41:22 +0900

    Add Perf.initialize()

--- majuro/src2/mh/majuro/mesa/Perf.java
+++ majuro/src2/mh/majuro/mesa/Perf.java
@@ -27,33 +27,55 @@ public class Perf {
 	
 	public static long codeCacheHit      = 0;
 	public static long codeCacheMiss     = 0;
+	
+	public static void initialize() {
+		fetchPage         = 0;
+		storePage         = 0;
+
+		cacheHit          = 0;
+		cacheMissConflict = 0;
+		cacheMissEmpty    = 0;
+		
+		fetch             = 0;
+		store             = 0;
+		readDbl           = 0;
+		
+		fetchMDS          = 0;
+		storeMDS          = 0;
+		readDblMDS        = 0;
+
+		readCode          = 0;
+		
+		codeCacheHit      = 0;
+		codeCacheMiss     = 0;
+	}
 
 
-	public static void stats() {		
-		logger.info("{}", String.format("fetchPage          %10u", fetchPage));
-		logger.info("{}", String.format("storePage          %10u", storePage));
+	public static void stats() {
+		logger.info("{}", String.format("fetchPage          %10d", fetchPage));
+		logger.info("{}", String.format("storePage          %10d", storePage));
 
 		long cacheMiss  = cacheMissEmpty + cacheMissConflict;
 		long cacheTotal = cacheMiss + cacheHit;
-		logger.info("{}", String.format("cacheHit           %10u", cacheHit));
-		logger.info("{}", String.format("cacheMissEmpty     %10u", cacheMissEmpty));
-		logger.info("{}", String.format("cacheMissConflict  %10u", cacheMissConflict));
-		logger.info("{}", String.format("cacheMiss          %10u", cacheMiss));
-		logger.info("{}", String.format("cacheRatio         %6.2f%%", (double)cacheHit / (double)cacheTotal));
+		logger.info("{}", String.format("cacheHit           %10d", cacheHit));
+		logger.info("{}", String.format("cacheMissEmpty     %10d", cacheMissEmpty));
+		logger.info("{}", String.format("cacheMissConflict  %10d", cacheMissConflict));
+		logger.info("{}", String.format("cacheMiss          %10d", cacheMiss));
+		logger.info("{}", String.format("cacheRatio         %10.2f%%", (double)cacheHit / (double)cacheTotal));
 
-		logger.info("{}", String.format("fetch              %10u", fetch));
-		logger.info("{}", String.format("store              %10u", store));
-		logger.info("{}", String.format("readDbl            %10u", readDbl));
+		logger.info("{}", String.format("fetch              %10d", fetch));
+		logger.info("{}", String.format("store              %10d", store));
+		logger.info("{}", String.format("readDbl            %10d", readDbl));
 
-		logger.info("{}", String.format("fetchMDS           %10u", fetchMDS));
-		logger.info("{}", String.format("storeMDS           %10u", storeMDS));
-		logger.info("{}", String.format("readDblMDS         %10u", readDblMDS));
+		logger.info("{}", String.format("fetchMDS           %10d", fetchMDS));
+		logger.info("{}", String.format("storeMDS           %10d", storeMDS));
+		logger.info("{}", String.format("readDblMDS         %10d", readDblMDS));
 		
-		logger.info("{}", String.format("readCode           %10u", readCode));
+		logger.info("{}", String.format("readCode           %10d", readCode));
 		
 		long codeCacheTotal = codeCacheHit + codeCacheMiss;
-		logger.info("{}", String.format("codeCacheHit       %10u", codeCacheHit));
-		logger.info("{}", String.format("codeCacheMiss      %10u", codeCacheMiss));
-		logger.info("{}", String.format("coddCacheRatio     %6.2f%%", (double)(codeCacheHit / codeCacheTotal)));
+		logger.info("{}", String.format("codeCacheHit       %10d", codeCacheHit));
+		logger.info("{}", String.format("codeCacheMiss      %10d", codeCacheMiss));
+		logger.info("{}", String.format("coddCacheRatio     %10.2f%%", (double)(codeCacheHit / codeCacheTotal)));
 	}
 }
