39a60227cd8a04cf484dfaf9f84b2f2b1fdcac68
hasegawa <hasegawa@ubuntu-2004.lan>
2020-05-15 20:36:16 +0900

    Add more test cases.

--- majuro/src2/mh/majuro/test/TestMemory.java
+++ majuro/src2/mh/majuro/test/TestMemory.java
@@ -532,5 +532,152 @@ public class TestMemory extends TestBase {
 			assertEquals(value, word & 0x00FF);
 		}
 	}
+	
+	@Test
+	public void testFieldMask() {
+		assertEquals(0x0001, Memory.fieldMask(0));
+		assertEquals(0x0003, Memory.fieldMask(1));
+		assertEquals(0x0007, Memory.fieldMask(2));
+		assertEquals(0x000F, Memory.fieldMask(3));
+		assertEquals(0x001F, Memory.fieldMask(4));
+		assertEquals(0x003F, Memory.fieldMask(5));
+		assertEquals(0x007F, Memory.fieldMask(6));
+		assertEquals(0x00FF, Memory.fieldMask(7));
+		assertEquals(0x01FF, Memory.fieldMask(8));
+		assertEquals(0x03FF, Memory.fieldMask(9));
+		assertEquals(0x07FF, Memory.fieldMask(10));
+		assertEquals(0x0FFF, Memory.fieldMask(11));
+		assertEquals(0x1FFF, Memory.fieldMask(12));
+		assertEquals(0x3FFF, Memory.fieldMask(13));
+		assertEquals(0x7FFF, Memory.fieldMask(14));
+		assertEquals(0xFFFF, Memory.fieldMask(15));
+	}
+	
+	@Test
+	public void testReadField() {
+		int source = 0xcafe;
+		// spec8 = pos (4bits) and size (4bits)
+		
+		int d1 = Memory.readField(source, 0x03);
+		int d2 = Memory.readField(source, 0x43);
+		int d3 = Memory.readField(source, 0x83);
+		int d4 = Memory.readField(source, 0xc3);
+		//
+		int d23   = Memory.readField(source, 0x47);
+		int d1234 = Memory.readField(source, 0x0f);
+		
+		assertEquals(0xc, d1);
+		assertEquals(0xa, d2);
+		assertEquals(0xf, d3);
+		assertEquals(0xe, d4);
+		//
+		assertEquals(0xaf,     d23);
+		assertEquals(source, d1234);
+	}
+	
+	@Test
+	public void testWriteField() {
+		int dest = 0xcafe;
+		
+		int t1 = Memory.writeField(dest, 0x03, 1);
+		int t2 = Memory.writeField(dest, 0x43, 2);
+		int t3 = Memory.writeField(dest, 0x83, 3);
+		int t4 = Memory.writeField(dest, 0xc3, 4);
+		//
+		int t23 = Memory.writeField(dest, 0x47, 0xac);
+		int t1234 = Memory.writeField(dest, (byte)0x0f, 0xacde);
+		
+		assertEquals(0x1afe, t1);
+		assertEquals(0xc2fe, t2);
+		assertEquals(0xca3e, t3);
+		assertEquals(0xcaf4, t4);
+		
+		assertEquals(0xcace, t23);
+		assertEquals(0xacde, t1234);
+	}
+
+	@Test
+	public void testLengthenPDA() {
+		int value       = 0x1234;
+
+		{
+			int actual = Memory.lengthenPDA(value);
+			int expect = Memory.PDA + value;
+			
+			assertEquals(expect, actual);
+		}
+	}
+
+	@Test
+	public void testOffsetPDA() {
+		int value       = Memory.PDA + 0x1234;
+
+		{
+			int actual = Memory.offsetPDA(value);
+			int expect = value - Memory.PDA;
+			
+			assertEquals(expect, actual);
+		}
+	}
+	
+	@Test
+	public void testFetchPDA() {
+		int pointer = 0x5678;
+		int value   = 65432;
+		
+		int     longPointer = Memory.lengthenPDA(pointer);
+		short[] page        = Memory.rawPage(longPointer);
+		
+		fill(page, 0);
+		
+		// Before
+		{
+			int actual = Memory.fetchPDA(pointer);
+			assertNotEquals(value, actual);
+		}
+		
+		Memory.storePDA(pointer, value);
+		
+		// After
+		{
+			int pageOffset = longPointer & Memory.PAGE_OFFSET_MASK;
+			int actual= page[pageOffset] & 0xFFFF;
+			assertEquals(value, actual);
+		}
+		{
+			int actual = Memory.fetchPDA(pointer);
+			assertEquals(value, actual);
+		}
+	}
+	
+	@Test
+	public void testStorePDA() {
+		int pointer = 0x5678;
+		int value   = 65432;
+		
+		int     longPointer = Memory.lengthenPDA(pointer);
+		short[] page        = Memory.rawPage(longPointer);
+		
+		fill(page, 0);
+		
+		// Before
+		{
+			int actual = Memory.fetchPDA(pointer);
+			assertNotEquals(value, actual);
+		}
+		
+		Memory.storePDA(pointer, value);
+		
+		// After
+		{
+			int pageOffset = longPointer & Memory.PAGE_OFFSET_MASK;
+			int actual= page[pageOffset] & 0xFFFF;
+			assertEquals(value, actual);
+		}
+		{
+			int actual = Memory.fetchPDA(pointer);
+			assertEquals(value, actual);
+		}
+	}
 
 }
