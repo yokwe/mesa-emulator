81f626b2f5810ad2a30511aa479d67b83d4051de
hasegawa <hasegawa@ubuntu-2004.lan>
2020-05-15 11:40:19 +0900

    Add more annotations PDA_POINTER, FIELD_SPEC, FIELD_DESC

--- majuro/src2/mh/majuro/mesa/Type.java
+++ majuro/src2/mh/majuro/mesa/Type.java
@@ -42,23 +42,44 @@ public final class Type {
 
 	@Retention(RetentionPolicy.SOURCE)
 	@Target({ElementType.TYPE_USE})
+	public static @interface PDA_POINTER {
+	}
+	
+	@Retention(RetentionPolicy.SOURCE)
+	@Target({ElementType.TYPE_USE})
 	public static @interface PAGE_NUMBER {
 	}
 
+	//FieldSpec: TYPE = MACHINE DEPENDENT RECORD [
+	// pos (0:0..3):  NIBBLE,
+	// size (0:4..7): NIBBLE];
+	@Retention(RetentionPolicy.SOURCE)
+	@Target({ElementType.TYPE_USE})
+	public static @interface FIELD_SPEC {
+	}
+
+	//FieldDesc: TYPE = MACHINE DEPENDENT RECORD [
+    //  offset(0:0..7) : BYTE,
+    //  field(0:8..15) : FieldSpec];
+	@Retention(RetentionPolicy.SOURCE)
+	@Target({ElementType.TYPE_USE})
+	public static @interface FIELD_DESC {
+	}
+
 	
-	// NibblePair: TYPE = MACHINE DEPENDENT RECORD[left (0:0..3) NIBBLE, right(0:4..7) NIBBLE];
-	public static int nibblePairLeft(@CARD16 int value) {
-		return (value >>> 4) & 0x0F;
-	}
-	public static int nibblePiarRight(@CARD16 int value) {
-		return value & 0x0F;
-	}
+//	// NibblePair: TYPE = MACHINE DEPENDENT RECORD[left (0:0..3) NIBBLE, right(0:4..7) NIBBLE];
+//	public static @CARD16 int nibblePairLeft(@CARD16 int value) {
+//		return (value >>> 4) & 0x0F;
+//	}
+//	public static @CARD16 int nibblePiarRight(@CARD16 int value) {
+//		return value & 0x0F;
+//	}
 	
 	// BytePair: TYPE =  MACHINE DEPENDENT RECORD [left (0: 0..7), right (0: 8..15): BYTE];
-	public static int bytePairLeft(@CARD32 int value) {
+	public static @CARD8 int bytePairLeft(@CARD16 int value) {
 		return (value >>> 8) & 0xFF;
 	}
-	public static int bytePiarRight(@CARD32 int value) {
+	public static @CARD8 int bytePairRight(@CARD16 int value) {
 		return value & 0xFF;
 	}
 }
