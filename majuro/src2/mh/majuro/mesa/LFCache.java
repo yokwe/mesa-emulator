c1243fd8d1f3023fb73fd6e25cc9e089ef2a84fa
hasegawa <hasegawa@ubuntu-2004.lan>
2020-05-17 20:34:20 +0900

    Accept negative index

--- majuro/src2/mh/majuro/mesa/LFCache.java
+++ majuro/src2/mh/majuro/mesa/LFCache.java
@@ -1,5 +1,6 @@
 package mh.majuro.mesa;
 
+import mh.majuro.UnexpectedException;
 import mh.majuro.mesa.Type.CARD16;
 import mh.majuro.mesa.Type.POINTER;
 
@@ -18,24 +19,34 @@ public final class LFCache {
 	public static @POINTER int getLF() {
 		return lf;
 	}
-	public static @CARD16 int fetchLF(@CARD16 int index) {
-		int pageIndex = offset + (index & 0xFFFF);
-		if (pageIndex <= pageEnd) {
-			if (Perf.ENABLE) Perf.lfCacheHit++;
-			return page[pageIndex] & 0xFFFF;
+	public static @CARD16 int fetch(int index) {
+		// NOTE index can be negative value to access LocalOverhead
+		int pageIndex = offset + index;
+		if (0 <= pageIndex) {
+			if (pageIndex <= pageEnd) {
+				if (Perf.ENABLE) Perf.lfCacheHit++;
+				return page[pageIndex] & 0xFFFF;
+			} else {
+				if (Perf.ENABLE) Perf.lfCacheMiss++;
+				return Memory.fetchMDS(lf + index);
+			}
 		} else {
-			if (Perf.ENABLE) Perf.lfCacheMiss++;
-			return Memory.fetchMDS(lf + index);
+			throw new UnexpectedException("");
 		}
 	}
-	public static void storeLF(@CARD16 int index, @CARD16 int newValue) {
-		int pageIndex = offset + (index & 0xFFFF);
-		if (pageIndex <= pageEnd) {
-			if (Perf.ENABLE) Perf.lfCacheHit++;
-			page[pageIndex] = (short)newValue;
+	public static void store(@CARD16 int index, int newValue) {
+		// NOTE index can be negative value to access LocalOverhead
+		int pageIndex = offset + index;
+		if (0 <= pageIndex) {
+			if (pageIndex <= pageEnd) {
+				if (Perf.ENABLE) Perf.lfCacheHit++;
+				page[pageIndex] = (short)newValue;
+			} else {
+				if (Perf.ENABLE) Perf.lfCacheMiss++;
+				Memory.storeMDS(lf + index, newValue);
+			}
 		} else {
-			if (Perf.ENABLE) Perf.lfCacheMiss++;
-			Memory.storeMDS(lf + index, newValue);
+			throw new UnexpectedException("");
 		}
 	}
 
